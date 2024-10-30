fbd06f669e15d526162026fdc18b9a365a1577f7
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-10-30 17:46:53 -0300

    feat(): cria as funcoes de estudo

---
+++ build/tiposFuncoes.js
@@ -0,0 +1,13 @@
+"use strict";
+//FUNÇÕES
+function addNumber(x, y) {
+    return x + y;
+}
+;
+let soma = addNumber(3, 4);
+console.log(soma);
+function addToHello(name) {
+    return `Hello ${name}`;
+}
+;
+console.log(addToHello);
---
+++ src/tiposFuncoes.ts
@@ -0,0 +1,15 @@
+//FUNÇÕES
+
+function addNumber(x: number, y: number) : number {
+    return x + y;
+};
+
+let soma : number = addNumber(3, 4);
+
+console.log(soma);
+
+function addToHello (name : string) : string {
+    return `Hello ${name}`;
+};
+
+console.log(addToHello);
