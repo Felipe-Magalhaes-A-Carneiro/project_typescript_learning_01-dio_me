f70683e27c1f89229d8cb933506a00ad9770caf8
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-10-31 16:00:08 -0300

    feat(): cria funcao async para estudo

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,19 @@ let soma : number = addNumber(3, 4);
 
 console.log(soma);
 
+// FUNÇÃO SIMPLES
 function addToHello (name : string) : string {
     return `Hello, ${name}`;
 };
 
+// FUNÇÃO MULTI TIPOS
 function CallToPhone (phone : number | string): number | string {
     return phone
+};
+
+// FUNÇÃO ASYNC
+async function getDatabase(id:number): Promise<string> {
+    return "Felipe";
 };
 
 console.log(addToHello("Felipe"));
